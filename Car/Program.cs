c485399226fc1844f214850e5391b5b9b18f6e18
yasinvalipoor <yasinvalipoor14@gmail.com>
2024-06-14 15:59:33 +0330

    Add project files.

---
+++ Car/Program.cs
@@ -0,0 +1,110 @@
+﻿
+
+CarAbstract benz = new Benz();
+Console.WriteLine($"{benz.Name()} - ${benz.Cost()}");
+
+benz = new DarkCover(benz);
+benz = new BackFireRemapEngine(benz);
+
+Console.WriteLine($"{benz.Name()} - ${benz.Cost()}");
+
+
+
+
+public abstract class CarAbstract
+{
+    public abstract string Name();
+    public abstract int Cost();
+}
+
+
+public class Benz : CarAbstract
+{
+    public override int Cost()
+    {
+        return 1000;
+    }
+
+    public override string Name()
+    {
+        return "Benz SLS";
+    }
+}
+
+public class BMW : CarAbstract
+{
+    public override int Cost()
+    {
+        return 500;
+    }
+
+    public override string Name()
+    {
+        return "BMW i4";
+    }
+}
+
+public abstract class OptionalFeatureAbstract : CarAbstract
+{
+    protected readonly CarAbstract _car;
+
+    public OptionalFeatureAbstract(CarAbstract car)
+    {
+        this._car = car;
+    }
+
+    public override int Cost()
+    {
+        return _car.Cost();
+    }
+
+    public override string Name()
+    {
+        return _car.Name();
+    }
+}
+
+
+
+
+
+public class DarkCover : OptionalFeatureAbstract
+{
+    public DarkCover(CarAbstract car) : base(car) { }
+
+    public override int Cost()
+    {
+        return _car.Cost() + 1000;
+    }
+
+    public override string Name()
+    {
+        return _car.Name() + " + Dark Cover";
+    }
+}
+
+
+public class BackFireRemapEngine : OptionalFeatureAbstract
+{
+    public BackFireRemapEngine(CarAbstract car) : base(car) { }
+
+    public override int Cost()
+    {
+        return _car.Cost() + 5000;
+    }
+
+    public override string Name()
+    {
+        return _car.Name() + " + Remap BackFire";
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Coffee/Program.cs
@@ -0,0 +1,135 @@
+﻿using DecoratorPattern;
+using System;
+
+namespace DecoratorPattern
+{
+    // Component
+    public abstract class Coffee
+    {
+        public abstract string GetDescription();
+        public abstract double GetCost();
+    }
+
+    // ConcreteComponent
+    public class SimpleCoffee : Coffee
+    {
+        public override string GetDescription()
+        {
+            return "Simple Coffee";
+        }
+
+        public override double GetCost()
+        {
+            return 5.0;
+        }
+    }
+
+    // Decorator
+    public abstract class CoffeeDecorator : Coffee
+    {
+        protected Coffee _coffee;
+
+        public CoffeeDecorator(Coffee coffee)
+        {
+            _coffee = coffee;
+        }
+
+        public override string GetDescription()
+        {
+            return _coffee.GetDescription();
+        }
+
+        public override double GetCost()
+        {
+            return _coffee.GetCost();
+        }
+    }
+
+    // ConcreteDecorator
+    public class Milk : CoffeeDecorator
+    {
+        public Milk(Coffee coffee) : base(coffee)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return _coffee.GetDescription() + ", Milk";
+        }
+
+        public override double GetCost()
+        {
+            return _coffee.GetCost() + 1.5;
+        }
+    }
+
+    // ConcreteDecorator
+    public class Sugar : CoffeeDecorator
+    {
+        public Sugar(Coffee coffee) : base(coffee)
+        {
+        }
+
+        public override string GetDescription()
+        {
+            return _coffee.GetDescription() + ", Sugar";
+        }
+
+        public override double GetCost()
+        {
+            return _coffee.GetCost() + 0.5;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Order a simple coffee
+            Coffee myCoffee = new SimpleCoffee();
+            Console.WriteLine($"{myCoffee.GetDescription()} - ${myCoffee.GetCost()}");
+
+            // Add milk
+            myCoffee = new Milk(myCoffee);
+            Console.WriteLine($"{myCoffee.GetDescription()} - ${myCoffee.GetCost()}");
+
+            // Add sugar
+            myCoffee = new Sugar(myCoffee);
+            Console.WriteLine($"{myCoffee.GetDescription()} - ${myCoffee.GetCost()}");
+
+            // Add another sugar
+            myCoffee = new Sugar(myCoffee);
+            Console.WriteLine($"{myCoffee.GetDescription()} - ${myCoffee.GetCost()}");
+        }
+    }
+}
+
+
+/*
+ * 
++----------------------------+     +----------------------------+
+|  <<abstract>>              |     |  SimpleCoffee              |
+|     Coffee                 |<----|----------------------------|
+|----------------------------|     | +GetDescription() : string |
+| +GetDescription() : string |     | +GetCost() : double        |
+| +GetCost() : double        |     +----------------------------+
++----------------------------+
+       ^
+       |
+       |
+       +----------------------------------------+
+       |                                        |
++----------------------------+     +----------------------------+
+|  <<abstract>>              |     |      Milk                  |
+| CoffeeDecorator            |<----|----------------------------|
+|----------------------------|     | +GetDescription() : string |
+| -coffee : Coffee           |     | +GetCost() : double        |
+| +GetDescription() : string |     +----------------------------+
+| +GetCost() : double        |
++----------------------------+     +---------------------------+
+                         |         |      Sugar                |
+                         ----------|---------------------------|
+                                   | +GetDescription() : string|
+                                   | +GetCost() : double       |
+                                   +---------------------------+
+ */
